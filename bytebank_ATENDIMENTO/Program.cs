44a3fd8b0aa242e1a1a378b5f5aeac001648cbbe
Vinicius Mello <153565970+ViniciusFelicianoMello@users.noreply.github.com>
2024-03-23 12:04:17 -0300

    projeto byte bank

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,278 @@
+﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Exceptions;
+using bytebank_ATENDIMENTO.bytebank.Util;
+using System.Collections;
+
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+#region exemplos arrays em c#
+//TestaArrayInt();
+//TestaBuscarPalavra();
+
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do array {idades.Length}");
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Indice = [{i}] = {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
+
+void TestaBuscarPalavra()
+{
+    string[] ArrayPalavras = new string[5];
+
+    for (int i = 0; i < ArrayPalavras.Length; i++)
+    {
+        Console.Write($"Digite {i+1}ª palavra: ");
+        ArrayPalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser encontrada: ");
+    var Busca = Console.ReadLine();
+
+    foreach (string Palavra in ArrayPalavras)
+    {
+        if (Palavra.Equals(Busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {Busca}");
+            break;
+        }
+    }
+}
+
+
+
+Array amostra = new double[5]; /*Array.CreateInstance(typeof(double), 5);*/
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+//[5,9][1,8][7,1][10][6,9]
+
+//TestaMediana(amostra);
+void TestaMediana(Array array)
+{
+    if ((array == null)||(array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo mediana está vazio ou nulo");
+    }
+
+    double[] numerosOrdenados = (double [])array.Clone();
+    Array.Sort(numerosOrdenados);
+    //[1,8][5,9][6,9][7,1][10]
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio]:
+        numerosOrdenados[meio] + numerosOrdenados[meio - 1] / 2;
+
+    Console.WriteLine($"Com base na amostra a media ={mediana}");
+}
+
+void TestaArrayDeCC()
+{
+    ListaDeCC listaDeContas = new ListaDeCC();
+    listaDeContas.Adicionar(new ContaCorrente(874, "5679787-A"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "4456668-B"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    var ContaDoVinicius = new ContaCorrente(367, "7171778-V");
+    listaDeContas.Adicionar(ContaDoVinicius);
+    //listaDeContas.ExibirLista();
+    //Console.WriteLine("================");
+    //listaDeContas.Remover(ContaDoVinicius);
+    //listaDeContas.ExibirLista();
+
+    for (int i = 0; i < listaDeContas.Tamanho; i++)
+    {
+        ContaCorrente conta = listaDeContas[i];
+        Console.WriteLine($"Indice [{i}] = {conta.Conta}/{conta.Numero_agencia}");
+    }
+}
+
+//TestaArrayDeCC();
+#endregion
+
+#region Exemplos de uso do list
+//Generica<int> teste1 = new Generica<int>();
+//teste1.MostrarMensagem(10);
+
+//Generica<string> teste2 = new Generica<string>();
+//teste2.MostrarMensagem("Olá mundo!");
+//public class Generica<T>
+//{
+//    public void MostrarMensagem(T t)
+//    {
+//        Console.WriteLine($"Exibindo {(t)}");
+//    }
+//}
+
+//List<ContaCorrente> listaDeContas2 = new List<ContaCorrente>()
+//{
+//    new ContaCorrente(874, "5679787-A"),
+//    new ContaCorrente(874, "4456668-B"),
+//    new ContaCorrente(874, "7781438-C")
+//};
+
+//List<ContaCorrente> listaDeContas3 = new List<ContaCorrente>()
+//{
+//    new ContaCorrente(951, "5679787-E"),
+//    new ContaCorrente(321, "4456668-F"),
+//    new ContaCorrente(719, "7781438-G")
+//};
+
+//listaDeContas2.AddRange(listaDeContas3);
+//listaDeContas2.Reverse();
+
+//for (int i = 0; i < listaDeContas2.Count; i++)
+//{
+//    Console.WriteLine($"Indice[{i}] = Conta [{listaDeContas2[i].Conta}]");
+//}
+
+//Console.WriteLine("\n\n");
+//var range = listaDeContas3.GetRange(0, 1);
+//for (int i = 0; i < range.Count; i++)
+//{
+//    Console.WriteLine($"Indice[{i}] = Conta [{range[i].Conta}]");
+//}
+
+//Console.WriteLine("\n\n");
+//listaDeContas3.Clear();
+//for (int i = 0; i < listaDeContas3.Count; i++)
+//{
+//    Console.WriteLine($"Indice[{i}] = Conta [{range[i].Conta}]");
+//}
+#endregion
+
+List<ContaCorrente> listaDeContas = new List<ContaCorrente>() {
+    new ContaCorrente(95, "123456-X") {Saldo=100},
+    new ContaCorrente(95, "951258-X") {Saldo=200},
+    new ContaCorrente(94, "987321-W") {Saldo=60}
+};
+
+
+AtendimentoCliente();
+void AtendimentoCliente()
+{
+    try
+    {
+        char opcao = '0';
+        while (opcao != '6')
+        {
+            Console.Clear();
+            Console.WriteLine("===============================");
+            Console.WriteLine("===       Atendimento       ===");
+            Console.WriteLine("===1 - Cadastrar Conta      ===");
+            Console.WriteLine("===2 - Listar Contas        ===");
+            Console.WriteLine("===3 - Remover Conta        ===");
+            Console.WriteLine("===4 - Ordenar Contas       ===");
+            Console.WriteLine("===5 - Pesquisar Conta      ===");
+            Console.WriteLine("===6 - Sair do Sistema      ===");
+            Console.WriteLine("===============================");
+            Console.WriteLine("\n\n");
+            Console.Write("Digite a opção desejada: ");
+            try
+            {
+                opcao = Console.ReadLine()[0];
+            }
+            catch (Exception excecao)
+            {
+                throw new ByteBankException(excecao.Message);
+            }
+            
+            switch (opcao)
+            {
+                case '1':
+                    CadastrarConta();
+                    break;
+                case '2':
+                    ListarConta();
+                    break;
+                default:
+                    Console.WriteLine("Opcao não implementada.");
+                    break;
+            }
+        }
+    }
+    catch (ByteBankException excecao)
+    {
+        Console.WriteLine($"{excecao.Message}");
+    }
+   
+}
+
+void CadastrarConta()
+{
+    Console.Clear();
+    Console.WriteLine("===============================");
+    Console.WriteLine("===   CADASTRO DE CONTAS    ===");
+    Console.WriteLine("===============================");
+    Console.WriteLine("\n");
+    Console.WriteLine("=== Informe dados da conta ===");
+    Console.Write("Número da conta: ");
+    string numeroConta = Console.ReadLine();
+
+    Console.Write("Número da Agência: ");
+    int numeroAgencia = int.Parse(Console.ReadLine());
+
+    ContaCorrente conta = new ContaCorrente(numeroAgencia, numeroConta);
+
+    Console.Write("Informe o saldo inicial: ");
+    conta.Saldo = double.Parse(Console.ReadLine());
+
+    Console.Write("Infome nome do Titular: ");
+    conta.Titular.Nome = Console.ReadLine();
+
+    Console.Write("Infome CPF do Titular: ");
+    conta.Titular.Cpf = Console.ReadLine();
+
+    Console.Write("Infome Profissão do Titular: ");
+    conta.Titular.Profissao = Console.ReadLine();
+
+    listaDeContas.Add(conta);
+    
+    Console.WriteLine("... Conta cadastrada com sucesso! ...");
+    Console.ReadKey();
+}
+
+void ListarConta()
+{
+    Console.Clear();
+    Console.WriteLine("===============================");
+    Console.WriteLine("===     LISTA DE CONTAS     ===");
+    Console.WriteLine("===============================");
+    Console.WriteLine("\n");
+    if (listaDeContas.Count <= 0)
+    {
+        Console.WriteLine("... Não há contas cadastradas! ...");
+        Console.ReadKey();
+        return;
+    }
+    foreach (ContaCorrente item in listaDeContas)
+    {
+        Console.WriteLine("===  Dados da Conta  ===");
+        Console.WriteLine("Número da Conta : " + item.Conta);
+        Console.WriteLine("Saldo da Conta : " + item.Saldo);
+        Console.WriteLine("Titular da Conta: " + item.Titular.Nome);
+        Console.WriteLine("CPF do Titular  : " + item.Titular.Cpf);
+        Console.WriteLine("Profissão do Titular: " + item.Titular.Profissao);
+        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        Console.ReadKey();
+    }
+}
+
---
+++ bytebank_ATENDIMENTO/bytebank.Util/ListaDeCC.cs
@@ -0,0 +1,112 @@
+﻿using bytebank.Modelos.Conta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ATENDIMENTO.bytebank.Util;
+
+public class ListaDeCC
+{
+    private ContaCorrente[] _itens = null;
+    private int ProximaPosicao = 0;
+    public ListaDeCC(int tamanhoInicial = 5)
+    {
+        _itens = new ContaCorrente[tamanhoInicial];
+    }
+
+    public void Adicionar(ContaCorrente item)
+    {
+        Console.WriteLine($"Adicionando item na posição {ProximaPosicao}");
+        VerificarCapacidade(ProximaPosicao + 1);
+        _itens[ProximaPosicao] = item;
+        ProximaPosicao++;
+    }
+
+    private void VerificarCapacidade(int TamanhoNecessario)
+    {
+        if (_itens.Length >= TamanhoNecessario)
+        {
+            return;
+        }
+        Console.WriteLine("\nCapacidade da Lista aumentada!");
+        ContaCorrente[] novoArray = new ContaCorrente[TamanhoNecessario];
+
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            novoArray[i] = _itens[i];
+        }
+
+        _itens = novoArray;
+    }
+
+    public void VerificaMaiorSaldo()
+    {
+        ContaCorrente? contaComMaiorSaldo = _itens[0];
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            if (_itens[i].Saldo > contaComMaiorSaldo?.Saldo)
+            {
+                contaComMaiorSaldo = _itens[i];
+            }
+        }
+        Console.WriteLine($"A conta com maior saldo é de R${contaComMaiorSaldo?.Saldo}");
+    }
+
+    public void Remover(ContaCorrente conta)
+    {
+        int indiceItem = -1;
+        for (int i = 0; i < ProximaPosicao; i++)
+        {
+            ContaCorrente contaAtual = _itens[i];
+            if (contaAtual == conta)
+            {
+                indiceItem = i;
+                break;
+            }
+        }
+        for (int i = indiceItem; i < ProximaPosicao - 1; i++)
+        {
+            _itens[i] = _itens[i + 1];
+        }
+        ProximaPosicao--;
+        _itens[ProximaPosicao] = null;
+    }
+
+    public void ExibirLista()
+    {
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            if (_itens[i] != null)
+            {
+                var conta = _itens[i];
+                Console.WriteLine($" Indice[{i}] = Conta:{conta.Conta} - N° da Agência: {conta.Numero_agencia}" );
+            }
+        }
+    }
+
+    public ContaCorrente RecuperarItemIndice(int indice)
+    {
+        if (indice < 0||indice >=ProximaPosicao)
+        {
+            throw new ArgumentOutOfRangeException(nameof(indice));
+        }
+        return _itens[indice];
+    }
+
+    public int Tamanho {
+        get
+        {
+            return ProximaPosicao;
+        }
+    }
+
+    public ContaCorrente this[int indice]
+    {
+        get
+        {
+            return RecuperarItemIndice(indice);
+        }
+    }
+}
